96e48aeaf22e2829295f3666455ddf4a5f8731ba
Lukheo <theolucas200272@gmail.com>
2024-02-15 12:27:58 +0100

    first ver : add header and basic css

---
+++ server.js
@@ -0,0 +1,34 @@
+const http = require('node:http');
+const express = require('express') ;
+const { engine } = require('express-handlebars') ;
+const path = require('path');
+
+
+
+
+const hostname = '127.0.0.1';
+const port = 3000;
+const app = express();
+
+app.engine('hbs', engine({extname: '.hbs'}));
+app.set('view engine', 'hbs');
+app.set('views', './views');
+app.use(express.static(path.join(__dirname, '../public')));
+app.use('/css', express.static(path.join(__dirname, 'public/css')));
+app.use('/images', express.static(path.join(__dirname, 'public/images')));
+
+
+app.get('/', (req, res) => {
+    res.render('accueil');
+});
+app.get('/connexion',(req, res) => {
+    res.render('connexion');
+});
+
+app.get('/actualites',(req, res) => {
+    res.render('actualites');
+});
+
+app.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
